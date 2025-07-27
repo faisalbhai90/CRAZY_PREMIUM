61a2c5f31c48045c4816316e2e8d042d942b09c8
faisalbhai90 <nahidfaisalrony@gmail.com>
2025-07-27 16:25:51 +0600

    Update Sniper_Scope.cs

--- Sniper_Scope.cs
+++ Sniper_Scope.cs
@@ -1,1 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class MOUSE_SCOPE
+{
+    public static List<long> addrs = new List<long>();
+    public static string orig = "FF FF FF FF 08 00 00 00 00 00 60 40 CD CC 8C 3F 8F C2 F5 3C CD CC CC 3D 06 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 33 33 13 40 00 00 B0 3F";
+    public static string patch = "FF FF FF FF 08 00 00 00 00 00 60 40 CD CC 8C 3F 8F C2 F5 3C CD CC CC 3D 06 00 00 00 00 00 00 3F 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 33 33 13 40";
+
+    public static async Task Run(dynamic mem, dynamic PID)
+    {
+        try
+        {
+            PID.Text = "🔍 Loading Sniper Scope...";
+            var procSet = mem.SetProcess(new string[] { "HD-Player" });
+
+            if (!procSet)
+            {
+                MessageBox.Show("❌ Process Not Found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var raw = await mem.AoBScan(orig);
+            addrs = ((IEnumerable<long>)raw).ToList();
+
+            if (addrs.Count > 0)
+            {
+                PID.Text = "✅ Sniper Scope Load Success";
+                Console.Beep(1000, 200);
+            }
+            else
+            {
+                PID.Text = "❌ Sniper Scope Failed";
+                MessageBox.Show("Sniper Scope Failed X", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        catch (Exception ex)
+        {
+            PID.Text = $"❌ Patch Failed: {ex.Message}";
+        }
+    }
+
+    public static async Task Toggle(dynamic mem)
+    {
+        if (addrs.Count == 0) return;
+        Scope(mem, true);
+        await Task.Delay(74); // delay
+        Scope(mem, false);
+    }
+
+    public static void Scope(dynamic mem, bool on)
+    {
+        if (addrs.Count == 0) return;
+        foreach (var a in addrs)
+            mem.AobReplace(a, on ? patch : orig);
+    }
+}
 
